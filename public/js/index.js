09127ef58b13b02100aad2108594ec9e747f41ea
Ameya <ameyaganore@gmail.com>
2020-08-04 19:02:20 +0530

    url_clicakble

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ fetch('/news').then((res) => {
     res.json().then((data) => {
         data.map((single) => {
             x+='<div class="col-md-4">'
-            x+='<div class="card mb-4 box-shadow" style="height: 400px;">'
+            x+='<a href='+single.url+'> <div class="card mb-4 box-shadow" style="height: 400px;">'
             x+='<img class="card-img-top"  alt="News" style="height: 225px; width: 100%; display: block;" src='+ single.urlToImage +'<data-holder-rendered="true">'
             x+='<div class="card-body">'
             x+='<h4 id="heading-1"><span id="title">'+single.title+'</span></h4>'
@@ -13,7 +13,7 @@ fetch('/news').then((res) => {
             x+='<div class="d-flex justify-content-between align-items-center">'
             x+='</div>'
             x+='</div>'
-            x+='</div>'
+            x+='</div> </a>'
             x+='</div>'
             document.getElementById('imp').innerHTML=x;
         })
